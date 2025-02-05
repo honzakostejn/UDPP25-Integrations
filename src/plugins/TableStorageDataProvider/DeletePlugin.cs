92047ae5b7e0790a69801494f2a7f1deb9d7acca
honzakostejn <kostejn@me.com>
2025-02-05 13:45:02 +0100

    adding plugin

---
+++ src/plugins/TableStorageDataProvider/DeletePlugin.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace TableStorageDataProvider
+{
+    /// <summary>
+    /// Plugin development guide: https://docs.microsoft.com/powerapps/developer/common-data-service/plug-ins
+    /// Best practices and guidance: https://docs.microsoft.com/powerapps/developer/common-data-service/best-practices/business-logic/
+    /// </summary>
+    public class DeletePlugin : PluginBase
+    {
+        public DeletePlugin(string unsecureConfiguration, string secureConfiguration)
+            : base(typeof(DeletePlugin))
+        {
+            // TODO: Implement your custom configuration handling
+            // https://docs.microsoft.com/powerapps/developer/common-data-service/register-plug-in#set-configuration-data
+        }
+
+        // Entry point for custom business logic execution
+        protected override void ExecuteDataversePlugin(ILocalPluginContext localPluginContext)
+        {
+            if (localPluginContext == null)
+            {
+                throw new ArgumentNullException(nameof(localPluginContext));
+            }
+
+            var context = localPluginContext.PluginExecutionContext;
+        }
+    }
+}
---
+++ src/plugins/TableStorageDataProvider/UpdatePlugin.cs
@@ -0,0 +1,30 @@
+using Microsoft.Xrm.Sdk;
+using System;
+
+namespace TableStorageDataProvider
+{
+    /// <summary>
+    /// Plugin development guide: https://docs.microsoft.com/powerapps/developer/common-data-service/plug-ins
+    /// Best practices and guidance: https://docs.microsoft.com/powerapps/developer/common-data-service/best-practices/business-logic/
+    /// </summary>
+    public class UpdatePlugin : PluginBase
+    {
+        public UpdatePlugin(string unsecureConfiguration, string secureConfiguration)
+            : base(typeof(UpdatePlugin))
+        {
+            // TODO: Implement your custom configuration handling
+            // https://docs.microsoft.com/powerapps/developer/common-data-service/register-plug-in#set-configuration-data
+        }
+
+        // Entry point for custom business logic execution
+        protected override void ExecuteDataversePlugin(ILocalPluginContext localPluginContext)
+        {
+            if (localPluginContext == null)
+            {
+                throw new ArgumentNullException(nameof(localPluginContext));
+            }
+
+            var context = localPluginContext.PluginExecutionContext;
+        }
+    }
+}
